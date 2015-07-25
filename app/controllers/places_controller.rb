4d6a4f956d65380b2f1b6e9d9823ca8db7cb8cd1
vagrant <vagrant@precise32.(none)>
2015-07-25 15:22:35 +0000

    connect users with places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,5 @@
 class PlacesController < ApplicationController
+	before_action :authenticate_user!, :only => [:new, :create]
 # create method
 	def index
 		# creates variable using @
@@ -11,7 +12,7 @@ def new
 end
 
 def create
-	Place.create(place_params)
+	current_user.places.create(place_params)
 	redirect_to root_path
 	end
 
